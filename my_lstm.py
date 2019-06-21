0649b00f67568d32d8c177c8eca67f7ad032a433
chingheng113 <chingheng113@gmail.com>
2019-06-21 16:50:14 -0400

    back up

--- my_lstm.py
+++ my_lstm.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import numpy as np
+import pickle
+import os
+current_path = os.path.dirname(__file__)
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from sklearn.model_selection import train_test_split
-
+from keras.layers.core import Activation, Dense
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
 
 data = pd.read_csv('carotid_101518_modified.csv')
+data = data.loc[0:20, :]
 data.dropna(subset=['CONTENT'], axis=0, inplace=True)
 text_arr = []
 label_arr = []
@@ -16,10 +24,13 @@ for index, row in data.iterrows():
     if '<BASE64>' not in doc:
         text_arr.append(doc)
         label_arr.append(label)
+text_arr = np.array(text_arr)
+label_arr = np.array(label_arr)
+n_class = label_arr[0].shape[0]
 
 tokenizer = Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;?@[\]^_`{|}~ ')
 tokenizer.fit_on_texts(text_arr)
-# print(tokenizer.word_counts)
+vocab_size = len(tokenizer.word_counts)+2
 # print( tokenizer.word_index)
 # print( tokenizer.word_docs)
 t2s = tokenizer.texts_to_sequences(text_arr)
@@ -28,6 +39,16 @@ t2s_pad = sequence.pad_sequences(t2s, maxlen=MAX_SENTENCE_LENGTH)
 
 x_train, x_test, Y_train, Y_test = train_test_split(t2s_pad, label_arr, test_size=0.2, random_state=42)
 
+model = Sequential()
+model.add(Embedding(vocab_size, 128, input_length=MAX_SENTENCE_LENGTH))
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(n_class))
+model.add(Activation("sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
+model.fit(x_train, Y_train, batch_size=32, epochs=1, validation_split=0.2)
+y_pred_p = model.predict(x_test)
 
+with open(current_path, 'wb') as file_pi:
+    pickle.dump(y_pred_p, file_pi)
 print('done')
