b70fe084e4d49a0e82f434afc439195233b7b8ac
Ching-Heng <chingheng113@gmail.com>
2019-09-17 14:19:13 -0400

    back up

--- performance_binary.py
+++ performance_binary.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 
 def plot_training_acc():
-    with open('history.pickle', 'rb') as f:
+    with open('history_binary.pickle', 'rb') as f:
         history = pickle.load(f)
         plt.plot(history['acc'])
         plt.plot(history['val_acc'])
@@ -20,7 +20,7 @@ def plot_training_acc():
 
 
 def plot_training_loss():
-    with open('history.pickle', 'rb') as f:
+    with open('history_binary.pickle', 'rb') as f:
         history = pickle.load(f)
         plt.plot(history['loss'])
         plt.plot(history['val_loss'])
@@ -31,22 +31,22 @@ def plot_training_loss():
         plt.show()
 
 
-with open('predict_y.pickle', 'rb') as file:
+with open('predict_y_binary.pickle', 'rb') as file:
     result =pickle.load(file)
-    predict_y_p = result[:,0:17]
-    true_y = result[:,17:].astype(int)
+    predict_y_p = result[:,0:2]
+    true_y = result[:,2:].astype(int)
     # true_y = (true_y == 1)
     plot_training_acc()
     plot_training_loss()
-    for i in range(0, 17):
+    for i in range(0, 2):
         p_label = predict_y_p[:, i]
         p_label_b = (p_label > 0.5).astype(int)
         t_label = true_y[:, i]
         fpr, tpr, thresholds = roc_curve(t_label, p_label)
         roc_auc = auc(fpr, tpr)
         print('===', i)
-        print('AUC = ', round(roc_auc, 3))
+        print('AUC = ', round(roc_auc, 4))
         # plt.plot(fpr, tpr, label='(AUC = %0.3f )' % roc_auc, lw=1, alpha=.8)
-        print(classification_report(t_label, p_label_b))
+        print(classification_report(t_label, p_label_b, digits=4))
         print(confusion_matrix(t_label, p_label_b))
     print('done')
