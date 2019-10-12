937f16dd587657ecaca17c8336cbbc36bbefae18
chingheng113 <chingheng113@gmail.com>
2019-10-12 17:41:05 -0400

    bk

--- part_of_clinicalBERT/see.py
+++ part_of_clinicalBERT/see.py
@@ -2,7 +2,7 @@ import os, sys
 from pathlib import Path
 sys.path.append(os.path.abspath(os.path.join('/data/linc9/carodit_nlp/')))
 
-current_path = os.path.dirname(__file__)
+current_path = os.getcwd()
 root_path = Path(current_path).parent
 
 print(current_path)
