7162ac8f7b6f5245ce9018420ffdf72e1c754d00
chingheng113 <chingheng113@gmail.com>
2019-10-11 20:24:37 -0400

    bk

--- part_of_clinicalBERT/carotid_lstm.py
+++ part_of_clinicalBERT/carotid_lstm.py
@@ -87,7 +87,7 @@ def main():
                         validation_split=0.2,
                         callbacks=[
                             ReduceLROnPlateau(factor=0.5, patience=int(config['epochs']/10), verbose=1),
-                            EarlyStopping(verbose=1, patience=int(config['epochs']/2)),
+                            EarlyStopping(verbose=1, patience=int(config['epochs']/10)),
                             ModelCheckpoint(os.path.join(current_path, model.name + '.h5'), save_best_only=True, verbose=1)
                         ])
     # History
