96ac05d958f6d4463fed7939a4834dbeee3eae72
chingheng113 <chingheng113@gmail.com>
2019-10-12 19:44:57 -0400

    bk

--- part_of_clinicalBERT/carotid_lstm.py
+++ part_of_clinicalBERT/carotid_lstm.py
@@ -98,14 +98,14 @@ def main():
                         callbacks=[
                             ReduceLROnPlateau(factor=0.5, patience=int(config['epochs']/10), verbose=1),
                             EarlyStopping(verbose=1, patience=int(config['epochs']/10)),
-                            ModelCheckpoint(os.path.join('./results', model.name + '.h5'), save_best_only=True, verbose=1)
+                            ModelCheckpoint(os.path.join(current_path, 'results', model.name + '.h5'), save_best_only=True, verbose=1)
                         ])
     # History
-    data_util.save_variable(history.history, os.path.join('./results', 'history.pickle'))
+    data_util.save_variable(history.history, os.path.join(current_path, 'results', 'history.pickle'))
     # result
     y_pred_p = model.predict(t2s_test_pad)
     result = np.concatenate((y_pred_p, Y_test), axis=1)
-    data_util.save_variable(result, os.path.join('./results', 'predict_y.pickle'))
+    data_util.save_variable(result, os.path.join(current_path, 'results', 'predict_y.pickle'))
     print('done')
 
 
