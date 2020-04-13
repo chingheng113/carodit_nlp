8724dbb8065747f6307750c43d203f49ace8578d
chingheng113 <chingheng113@gmail.com>
2020-04-12 22:58:04 -0400

    bk

---
+++ experiments/lstm/lstm_carotid.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import re
+import argparse
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+from keras.layers.core import Activation, Dense
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
+from pathlib import Path
+from carotid_data import data_util
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--round",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="Which data round will be using")
+    return parser
+
+
+# def text_preprocessing():
+#     text_arr = []
+#     label_arr = []
+#     for index, row in data.iterrows():
+#         label = row[['RCCA', 'REICA', 'RIICA', 'RACA', 'RMCA', 'RPCA', 'REVA', 'RIVA', 'BA',
+#                      'LCCA', 'LEICA', 'LIICA', 'LACA', 'LMCA', 'LPCA', 'LEVA', 'LIVA']].values
+#         corpus = row['CONTENT']
+#         if '<BASE64>' not in corpus:
+#             sentences = corpus.split('\n')
+#             processed_sentence = ''
+#             for sentence in sentences:
+#                 if len(re.findall(r'[\u4e00-\u9fff]+', sentence)) == 0:
+#                     # no chinese sentence
+#                     if re.search('(>\s*\d+|<\s*\d+)', sentence):
+#                         sentence = sentence.replace('>', ' greater ')
+#                         sentence = sentence.replace('<', ' less ')
+#                     sentence = sentence.replace('%', ' percent')
+#                     processed_sentence += sentence + ' '
+#             processed_sentence = re.sub(' +', ' ', processed_sentence)
+#             text_arr.append(processed_sentence)
+#             label_arr.append(label)
+#     text_arr = np.array(text_arr)
+
+def read_internal_data(n):
+    train_data = pd.read_csv('..', 'data', 'internal', 'round_'+n, 'training_'+n)
+    label_cols = list(train_data.columns)[2:-1]
+    test_data = pd.read_csv('..', 'data', 'internal', 'round_'+n, 'testing_'+n)
+    return train_data, test_data, label_cols
+
+
+def read_external_data(n):
+    test_data = pd.read_csv('..', 'data', 'external', 'round_'+n, 'testing_'+n)
+    label_cols = list(test_data.columns)[2:-1]
+    return test_data, label_cols
+
+
+def model_training(train, label_cols, round_n):
+    x_train = train[['processed_content']]
+    y_train = train[label_cols]
+    # tokenize
+    tokenizer = Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~ ')
+    tokenizer.fit_on_texts(x_train)
+    t2s_train = tokenizer.texts_to_sequences(x_train)
+    # padding
+    MAX_SENTENCE_LENGTH = max(t2s_train, key=len)
+    t2s_train_pad = sequence.pad_sequences(t2s_train, maxlen=MAX_SENTENCE_LENGTH)
+    # data_util.save_variable([t2s_train_pad, Y_train], 'training_data.pickle')
+    # config
+    config = dict()
+    config['batch_size'] = 32
+    config['epochs'] = 50
+    config['n_hidden'] = 64
+    config['n_class'] = len(label_cols)
+    config['input_dim'] = len(tokenizer.word_counts)+2
+    config['output_dim'] = 128
+    # model
+    model = Sequential(name='lstm_'+round_n)
+    model.add(Embedding(input_dim=config['input_dim'], output_dim=config['output_dim'], input_length=MAX_SENTENCE_LENGTH))
+    model.add(LSTM(config['n_hidden'], dropout=0.2, recurrent_dropout=0.2, return_sequences=True))
+    model.add(LSTM(int(config['n_hidden']/2), dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(config['n_class']))
+    model.add(Activation("sigmoid"))
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    history = model.fit(t2s_train_pad, y_train,
+                        batch_size=config['batch_size'],
+                        epochs=config['epochs'],
+                        validation_split=0.2,
+                        callbacks=[
+                            ReduceLROnPlateau(factor=0.5, patience=int(config['epochs']/10), verbose=1),
+                            EarlyStopping(verbose=1, patience=int(config['epochs']/10)),
+                            ModelCheckpoint(os.path.join('results', 'internal', 'round_'+round_n, model.name + '.h5'),
+                                            save_best_only=True, verbose=1)
+                        ])
+    # History
+    data_util.save_variable(history.history, os.path.join('results', 'internal', 'round_'+round_n, 'history_'+round_n+'.pickle'))
+    return tokenizer, model
+
+
+def model_testing(testing, label_cols, model, tokenizer, round_n, ex_in):
+    x_test = testing[['processed_content']]
+    y_test = testing[label_cols]
+    # tokenize
+    t2s_test = tokenizer.texts_to_sequences(x_test)
+    # padding
+    MAX_SENTENCE_LENGTH = max(t2s_test, key=len)
+    t2s_test_pad = sequence.pad_sequences(t2s_test, maxlen=MAX_SENTENCE_LENGTH)
+    # data_util.save_variable([t2s_test_pad, Y_test], 'testing_data.pickle')
+    y_pred_p = model.predict(t2s_test_pad)
+    for index, elem in enumerate(label_cols):
+        testing[elem + '_pred'] = y_pred_p[:, index]
+    testing.to_csv(os.path.join('results', ex_in, 'round_'+round_n, 'predict_y_'+round_n+'.csv'), index=False)
+    print('testing done')
+
+
+if __name__ == '__main__':
+    parser = setup_parser()
+    args = parser.parse_args()
+    round_nm = args.round
+
+    in_train_data, in_test_data, in_label_cols = read_internal_data(round_nm)
+    in_train_data = in_train_data.head(10)
+    trained_tokenizer, trained_model = model_training(in_train_data, in_label_cols, round_nm)
+    model_testing(in_test_data, in_label_cols, trained_model, trained_tokenizer, round_nm, 'internal')
+
+    ex_test_data, ex_label_cols = read_external_data(round_nm)
+    model_testing(ex_test_data, ex_label_cols, trained_model, trained_tokenizer, round_nm, 'external')
+
