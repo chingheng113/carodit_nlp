8de980b15b0e768246ba99e7759cc99f119e5828
chingheng113 <chingheng113@gmail.com>
2020-04-24 13:05:28 -0400

    bk

--- experiments/clinical_xlnet/performance_clinical_xlnet.py
+++ experiments/clinical_xlnet/performance_clinical_xlnet.py
@@ -8,19 +8,19 @@ import os
 in_cols = ['RCCA', 'REICA', 'RIICA', 'RACA', 'RMCA', 'RPCA', 'REVA', 'RIVA', 'BA', 'LCCA', 'LEICA', 'LIICA',
            'LACA', 'LMCA', 'LPCA', 'LEVA', 'LIVA']
 
-for in_col in in_cols:
-    aucs = []
-    for n in range(10):
-        # read_path = os.path.join('results_5', 'internal', 'round_'+str(n), 'predict_result.pickle')
-        read_path = os.path.join('results', 'external', 'predict_result_'+str(n)+'.pickle')
-        with open(read_path, 'rb') as file:
-            test_data =pickle.load(file)
-            predict_prob = test_data[in_col + '_pred']
-            true_label = test_data[in_col]
-            fpr, tpr, _ = roc_curve(true_label, predict_prob)
-            roc_auc = auc(fpr, tpr)
-            aucs.append(roc_auc)
-    print(in_col, np.mean(aucs))
+# for in_col in in_cols:
+#     aucs = []
+#     for n in range(10):
+#         read_path = os.path.join('results', 'internal', 'round_'+str(n), 'predict_result.pickle')
+#         # read_path = os.path.join('results', 'external', 'predict_result_'+str(n)+'.pickle')
+#         with open(read_path, 'rb') as file:
+#             test_data =pickle.load(file)
+#             predict_prob = test_data[in_col + '_pred']
+#             true_label = test_data[in_col]
+#             fpr, tpr, _ = roc_curve(true_label, predict_prob)
+#             roc_auc = auc(fpr, tpr)
+#             aucs.append(roc_auc)
+#     print(in_col, np.mean(aucs))
 
 
 # ROC curves should be used when there are roughly equal numbers of observations for each class.
@@ -35,13 +35,13 @@ for in_col in in_cols:
 
 
 # alone
-# read_path = os.path.join('results', 'internal', 'round_3', 'predict_result.pickle')
-# read_path = os.path.join('results_10', 'external', 'predict_result_1.pickle')
-# for in_col in in_cols:
-#     with open(read_path, 'rb') as file:
-#         test_data =pickle.load(file)
-#         predict_prob = test_data[in_col + '_pred']
-#         true_label = test_data[in_col]
-#         fpr, tpr, _ = roc_curve(true_label, predict_prob)
-#         roc_auc = auc(fpr, tpr)
-#         print(in_col, roc_auc)
+read_path = os.path.join('results', 'internal', 'round_0', 'predict_result.pickle')
+# read_path = os.path.join('results', 'external', 'predict_result_1.pickle')
+for in_col in in_cols:
+    with open(read_path, 'rb') as file:
+        test_data =pickle.load(file)
+        predict_prob = test_data[in_col + '_pred']
+        true_label = test_data[in_col]
+        fpr, tpr, _ = roc_curve(true_label, predict_prob)
+        roc_auc = auc(fpr, tpr)
+        print(in_col, roc_auc)
