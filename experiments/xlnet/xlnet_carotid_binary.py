a14750ac5c74e2462ab44433426c7c856fe1a96d
chingheng113 <chingheng113@gmail.com>
2020-05-05 23:11:43 -0400

    bk

--- experiments/xlnet/xlnet_carotid_binary.py
+++ experiments/xlnet/xlnet_carotid_binary.py
@@ -18,92 +18,93 @@ import argparse
 
 
 
-# def train(model, num_epochs, optimizer, train_dataloader, valid_dataloader,
-#           model_save_path, train_loss_set=[], valid_loss_set=[],
-#           lowest_eval_loss=None, start_epoch=0, device="cpu"):
-#     """
-#       Train the model and save the model with the lowest validation loss
-#     """
-#     model.to(device)
-#
-#     # trange is a tqdm wrapper around the normal python range
-#     for i in trange(num_epochs, desc="Epoch"):
-#         # if continue training from saved model
-#         actual_epoch = start_epoch + i
-#
-#         # Training
-#         # Set our model to training mode (as opposed to evaluation mode)
-#         model.train()
-#         # Tracking variables
-#         tr_loss = 0
-#         num_train_samples = 0
-#
-#         # Train the data for one epoch
-#         for step, batch in enumerate(train_dataloader):
-#             # Add batch to GPU
-#             batch = tuple(t.to(device) for t in batch)
-#             # Unpack the inputs from our dataloader
-#             b_input_ids, b_input_mask, b_labels = batch
-#             # Clear out the gradients (by default they accumulate)
-#             optimizer.zero_grad()
-#             # Forward pass
-#             loss = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
-#             # store train loss
-#             tr_loss += loss.item()
-#             num_train_samples += b_labels.size(0)
-#             # Backward pass
-#             loss.backward()
-#             # Update parameters and take a step using the computed gradient
-#             optimizer.step()
-#             #scheduler.step()
-#
-#         # Update tracking variables
-#         epoch_train_loss = tr_loss/num_train_samples
-#         train_loss_set.append(epoch_train_loss)
-#
-#         print("Train loss: {}".format(epoch_train_loss))
-#
-#         # Validation
-#
-#         # Put model in evaluation mode to evaluate loss on the validation set
-#         model.eval()
-#
-#         # Tracking variables
-#         eval_loss = 0
-#         num_eval_samples = 0
-#
-#         # Evaluate data for one epoch
-#         for batch in valid_dataloader:
-#             # Add batch to GPU
-#             batch = tuple(t.to(device) for t in batch)
-#             # Unpack the inputs from our dataloader
-#             b_input_ids, b_input_mask, b_labels = batch
-#             # Telling the model not to compute or store gradients,
-#             # saving memory and speeding up validation
-#             with torch.no_grad():
-#                 # Forward pass, calculate validation loss
-#                 loss = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
-#                 # store valid loss
-#                 eval_loss += loss.item()
-#                 num_eval_samples += b_labels.size(0)
-#
-#         epoch_eval_loss = eval_loss/num_eval_samples
-#         valid_loss_set.append(epoch_eval_loss)
-#         print("Valid loss: {}".format(epoch_eval_loss))
-#
-#         if lowest_eval_loss == None:
-#             lowest_eval_loss = epoch_eval_loss
-#             # save model
-#             save_model(model, model_save_path, actual_epoch,
-#                        lowest_eval_loss, train_loss_set, valid_loss_set)
-#         else:
-#             if epoch_eval_loss < lowest_eval_loss:
-#                 lowest_eval_loss = epoch_eval_loss
-#                 # save model
-#                 save_model(model, model_save_path, actual_epoch,
-#                            lowest_eval_loss, train_loss_set, valid_loss_set)
-#         print("\n")
-#     return model, train_loss_set, valid_loss_set
+def train(model, num_epochs, optimizer, train_dataloader, valid_dataloader,
+          model_save_path, train_loss_set=[], valid_loss_set=[],
+          lowest_eval_loss=None, start_epoch=0, device="cpu"):
+    """
+      Train the model and save the model with the lowest validation loss
+    """
+    model.to(device)
+
+    # trange is a tqdm wrapper around the normal python range
+    for i in trange(num_epochs, desc="Epoch"):
+        # if continue training from saved model
+        actual_epoch = start_epoch + i
+
+        # Training
+        # Set our model to training mode (as opposed to evaluation mode)
+        model.train()
+        # Tracking variables
+        tr_loss = 0
+        num_train_samples = 0
+
+        # Train the data for one epoch
+        for step, batch in enumerate(train_dataloader):
+            # Add batch to GPU
+            batch = tuple(t.to(device) for t in batch)
+            # Unpack the inputs from our dataloader
+            b_input_ids, b_input_mask, b_labels = batch
+            # Clear out the gradients (by default they accumulate)
+            optimizer.zero_grad()
+            # Forward pass
+            loss = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
+
+            # store train loss
+            tr_loss += loss.item()
+            num_train_samples += b_labels.size(0)
+            # Backward pass
+            loss.backward()
+            # Update parameters and take a step using the computed gradient
+            optimizer.step()
+            #scheduler.step()
+
+        # Update tracking variables
+        epoch_train_loss = tr_loss/num_train_samples
+        train_loss_set.append(epoch_train_loss)
+
+        print("Train loss: {}".format(epoch_train_loss))
+
+        # Validation
+
+        # Put model in evaluation mode to evaluate loss on the validation set
+        model.eval()
+
+        # Tracking variables
+        eval_loss = 0
+        num_eval_samples = 0
+
+        # Evaluate data for one epoch
+        for batch in valid_dataloader:
+            # Add batch to GPU
+            batch = tuple(t.to(device) for t in batch)
+            # Unpack the inputs from our dataloader
+            b_input_ids, b_input_mask, b_labels = batch
+            # Telling the model not to compute or store gradients,
+            # saving memory and speeding up validation
+            with torch.no_grad():
+                # Forward pass, calculate validation loss
+                loss = model(b_input_ids, attention_mask=b_input_mask, labels=b_labels)
+                # store valid loss
+                eval_loss += loss.item()
+                num_eval_samples += b_labels.size(0)
+
+        epoch_eval_loss = eval_loss/num_eval_samples
+        valid_loss_set.append(epoch_eval_loss)
+        print("Valid loss: {}".format(epoch_eval_loss))
+
+        if lowest_eval_loss == None:
+            lowest_eval_loss = epoch_eval_loss
+            # save model
+            save_model(model, model_save_path, actual_epoch,
+                       lowest_eval_loss, train_loss_set, valid_loss_set)
+        else:
+            if epoch_eval_loss < lowest_eval_loss:
+                lowest_eval_loss = epoch_eval_loss
+                # save model
+                save_model(model, model_save_path, actual_epoch,
+                           lowest_eval_loss, train_loss_set, valid_loss_set)
+        print("\n")
+    return model, train_loss_set, valid_loss_set
 
 
 def generate_predictions(model, df, num_labels, device="cpu", batch_size=32):
@@ -249,8 +250,8 @@ def get_train_validation_dataloader(batch_size, train, label_cols):
     X_train = torch.tensor(X_train)
     X_valid = torch.tensor(X_valid)
 
-    Y_train = torch.tensor(Y_train, dtype=torch.float32)
-    Y_valid = torch.tensor(Y_valid, dtype=torch.float32)
+    Y_train = torch.tensor(Y_train, dtype=torch.long)
+    Y_valid = torch.tensor(Y_valid, dtype=torch.long)
 
     train_masks = torch.tensor(train_masks, dtype=torch.long)
     valid_masks = torch.tensor(valid_masks, dtype=torch.long)
@@ -292,7 +293,7 @@ def model_training(train_data, label_cols, round_n):
 
     optimizer = AdamW(model.parameters(), lr=1e-5, weight_decay=0.01, correct_bias=False) # 2e-5
     start = time.time()
-    model, train_loss_set, valid_loss_set = model.train(
+    model, train_loss_set, valid_loss_set = train(model=model,
                                                   num_epochs=num_epochs,
                                                   optimizer=optimizer,
                                                   train_dataloader=train_dataloader,
