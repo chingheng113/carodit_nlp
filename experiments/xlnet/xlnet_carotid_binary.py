c4d45a9b75f2064b4c660af7a3df1f9fb11ed4a2
chingheng113 <chingheng113@gmail.com>
2020-05-06 00:03:55 -0400

    bk

--- experiments/xlnet/xlnet_carotid_binary.py
+++ experiments/xlnet/xlnet_carotid_binary.py
@@ -127,8 +127,6 @@ def generate_predictions(model, df, num_labels, device="cpu", batch_size=32):
         masks = masks.to(device)
         with torch.no_grad():
             outputs = model(input_ids=X, attention_mask=masks)
-            print('**********')
-            print(outputs)
             loss, logits = outputs[:2]
             logits = logits.sigmoid().detach().cpu().numpy()
             pred_probs = np.vstack([pred_probs, logits])
@@ -278,7 +276,7 @@ def get_train_validation_dataloader(batch_size, train, label_cols):
 
 
 def model_testing(trained_model, test_data, label_cols, exin, round_n):
-    pred_probs = generate_predictions(trained_model, test_data, len(label_cols), device="cuda", batch_size=batch_size)
+    pred_probs = generate_predictions(trained_model, test_data, 2, device="cuda", batch_size=batch_size)
     for index, elem in enumerate(label_cols):
         test_data[elem+'_pred'] = pred_probs[:, index]
     if exin == 'internal':
